872efdda12b10f108000b3c587de8762c8e0bd74
KHOA <aohkne@gmail.com>
2024-05-29 14:49:50 +0700

    2nd commit 'responsive for mobile'-khoa

--- js/course_JS.js
+++ js/course_JS.js
@@ -1,43 +1,58 @@
 // open-close menu
 // const icon = document.getElementsByClassName('fa-bars');
 let nav = document.querySelector('.menu-icon');
-let menu = document.querySelectorAll('.menu-item');
+let menu = document.querySelector('.menu');
+let menuContainer = document.querySelector('.menu-container');
+let videoContainer = document.querySelector('.video_container');
 
 nav.onclick = function () {
-    menu.forEach(function (item) {
-        if (item.style.display == 'none') {
-            // document.querySelector('.fa-bars').classList.add('fa-x');
-            item.style.display = 'block';
-        } else {
-            // document.querySelector('.fa-bars').classList.remove('fa-x');
-            item.style.display = 'none';
+    // menuContainer.classList.toggle('l-3', 'l-1');
+    if (menu.style.display == 'none') {
 
-        }
-    });
+        //menu
+        menu.style.display = 'block';
+        menuContainer.classList.add('l-3');
+        menuContainer.classList.remove('l-0');
+
+        //video
+        videoContainer.classList.add('l-9');
+        videoContainer.classList.remove('l-12');
+
+    } else {
+        //menu
+        menu.style.display = 'none';
+        menuContainer.classList.add('l-0');
+        menuContainer.classList.remove('l-3');
+
+        //video
+        videoContainer.classList.add('l-12');
+        videoContainer.classList.remove('l-9');
+    }
+
 };
 
-//open - close content
-let title = document.querySelectorAll('.menu-title');
+// //open - close content
+// let title = document.querySelectorAll('.menu-title');
 
-menu.forEach(function (element) {
-    let child = element.childNodes;
-    child.forEach(function (e) {
-        console.log(e.nodeName);
-        if (e.nodeName == "H1") {
-            e.onclick = function () {
-                child.forEach(function (item) {
-                    if (item.nodeName == "A") {
-                        if (item.style.display == 'none') {
-                            item.style.display = 'block';
-                        } else {
-                            item.style.display = 'none';
+// menu.forEach(function (element) {
+//     let child = element.childNodes;
+//     child.forEach(function (e) {
+//         console.log(e.nodeName);
+//         if (e.nodeName == "H1") {
+//             e.onclick = function () {
+//                 child.forEach(function (item) {
+//                     if (item.nodeName == "A") {
+//                         if (item.style.display == 'none') {
+//                             item.style.display = 'block';
+//                         } else {
+//                             item.style.display = 'none';
 
-                        }
-                    }
-                });
-            };
-        }
-    });
-    // child.onclick()
+//                         }
+//                     }
+//                 });
+//             };
+//         }
+//     });
+//     // child.onclick()
 
-});
+// });
